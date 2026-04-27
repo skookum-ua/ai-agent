59cd802f9092001f33cf0e6e88beb7283a801a33
skookum-ua <skookum.ua@gmail.com>
2026-04-27 18:23:56 +0000

    first

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    
+    print("Hello from ai-agent!")
+
+    load_dotenv()
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    
+    if api_key == None:
+        raise RuntimeError("no api key")
+    
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(model = "gemini-2.5-flash", contents = messages)
+    
+    if response.usage_metadata == None:
+        raise RuntimeError("no metadata, bad api request")
+    
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response: \n{response.text}")
+
+if __name__ == "__main__":
+    main()
